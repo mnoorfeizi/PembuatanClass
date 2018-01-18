49ff73d929f201631ad73738a40e91f474e02493
mnoorfeizi <32352478+mnoorfeizi@users.noreply.github.com>
2018-01-18 23:46:18 +0700

    untuk menampilkan output menggunakan prosedur ini

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -16,4 +16,18 @@ private:
     
 
 public:
+	
+	Jenissemut (string namasemut, string namalain, string namalatin, string karakter, 
+	int panjangtubuh, int batasumur )
+
+    {
+    	Jenissemut::namasemut= namasemut;
+        Jenissemut::namalain = namalain;
+        Jenissemut::namalatin = namalatin;
+        Jenissemut::karakter = karakter;
+        Jenissemut::panjangtubuh = panjangtubuh;
+        Jenissemut::batasumur = batasumur;
+        
+    }
+
 
